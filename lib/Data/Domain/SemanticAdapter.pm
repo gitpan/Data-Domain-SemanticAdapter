82298266956fcdc634f49b67fefa9dac838cf8af
Marcel Grünauer <marcel@cpan.org>
2010-03-25 22:43:04 +0000

    Import of MARCEL/Data-Domain-SemanticAdapter-1.100840 from CPAN.
    gitpan-cpan-distribution: Data-Domain-SemanticAdapter
    gitpan-cpan-version:      1.100840
    gitpan-cpan-path:         MARCEL/Data-Domain-SemanticAdapter-1.100840.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Data/Domain/SemanticAdapter.pm
+++ lib/Data/Domain/SemanticAdapter.pm
@@ -1,67 +1,51 @@
-package Data::Domain::SemanticAdapter;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Data::Domain::SemanticAdapter;
+our $VERSION = '1.100840';
+# ABSTRACT: Adapter for Data::Semantic objects
 use Carp;
 use UNIVERSAL::require;
-
-
-our $VERSION = '0.01';
-
-
-use base qw(
-    Data::Domain
-    Data::Inherited
-    Class::Accessor::Complex
+use parent qw(
+  Data::Domain
+  Data::Inherited
+  Class::Accessor::Complex
 );
-
-
 __PACKAGE__->mk_scalar_accessors(qw(adaptee));
 
-
 # sub adaptee() to be defined in subclasses
-
 use constant OPTIONS => ();
 
-
 sub new {
-    my $class = shift;
-    my $self = bless {}, $class;
-
+    my $class   = shift;
+    my $self    = bless {}, $class;
     my @options = (qw/-not_in/, $self->every_list('OPTIONS'));
-    my $parsed = Data::Domain::_parse_args(\@_, \@options);
-
+    my $parsed  = Data::Domain::_parse_args(\@_, \@options);
     while (my ($key, $value) = each %{ $parsed || {} }) {
         $self->{$key} = $value;
     }
-  
     if ($self->{-not_in}) {
-        @{ $self->{-not_in} || [] } > 0 or
-            croak "-not_in : needs an arrayref of values";
+        @{ $self->{-not_in} || [] } > 0
+          or croak "-not_in : needs an arrayref of values";
     }
-
     my $semantic_class_name = $self->semantic_class_name;
     $semantic_class_name->require;
     $self->adaptee($semantic_class_name->new($self->semantic_args));
-
     $self;
 }
 
-
 # Default; subclasses can redefine this. But it makes sense to keep the
 # Data::Domain::* and Data::Semantic::* namespaces in sync.
-
 sub semantic_class_name {
     my $self = shift;
     (my $semantic_class_name = ref $self) =~
-        s/^Data::Domain::/Data::Semantic::/;
+      s/^Data::Domain::/Data::Semantic::/;
     $semantic_class_name;
 }
 
-
 # Turn the options accepted because of OPTIONS() into args to be passed to the
 # adaptee constructor. Here we provide a sensibe default.
-
 sub semantic_args {
     my $self = shift;
     my %args;
@@ -72,48 +56,40 @@ sub semantic_args {
     %args;
 }
 
-
 sub _inspect {
     my ($self, $data) = @_;
-  
     $self->adaptee->is_valid($data)
-        or return $self->msg(INVALID => $data);
-
+      or return $self->msg(INVALID => $data);
     if (defined $self->{-not_in}) {
-        grep { $data eq $_} @{ $self->{-not_in} }
-            and return $self->msg(EXCLUSION_SET => $data);
+        grep { $data eq $_ } @{ $self->{-not_in} }
+          and return $self->msg(EXCLUSION_SET => $data);
     }
 }
 
-
 # mirror the Data::Semantic::Name namespace classes
-
 sub install_shortcuts {
-    my %map = @_;
+    my %map      = @_;
     my $call_pkg = (caller)[0];
     while (my ($domain, $class) = each %map) {
         no strict 'refs';
         my $domain_class_name = "Data::Domain::$class";
         $domain_class_name->require;
-        *{ "${call_pkg}::${domain}" } = sub { $domain_class_name->new(@_) };
+        *{"${call_pkg}::${domain}"} = sub { $domain_class_name->new(@_) };
     }
 }
-
-
 1;
 
 
 __END__
-
-
+=pod
 
 =head1 NAME
 
 Data::Domain::SemanticAdapter - Adapter for Data::Semantic objects
 
-=head1 SYNOPSIS
+=head1 VERSION
 
-    Data::Domain::SemanticAdapter->new;
+version 1.100840
 
 =head1 DESCRIPTION
 
@@ -124,67 +100,9 @@ It, and therefore all the subclasses, support a C<-not_in> options. If given,
 the data must be different from all values in the exclusion set, supplied
 as an arrayref.
 
-Data::Domain::SemanticAdapter inherits from L<Data::Domain>,
-L<Data::Inherited>, and L<Class::Accessor::Complex>.
-
-The superclass L<Data::Domain> defines these methods and functions:
-
-    Date(), Enum(), Int(), List(), Num(), One_of(), String(), Struct(),
-    Time(), Whatever(), _check_range(), _parse_args(), import(), inspect(),
-    messages(), msg(), node_from_path(), subclass()
-
-The superclass L<Data::Inherited> defines these methods and functions:
-
-    every_hash(), every_list(), flush_every_cache_by_key()
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    cluck(), flatten(), mk_abstract_accessors(), mk_array_accessors(),
-    mk_boolean_accessors(), mk_class_array_accessors(),
-    mk_class_hash_accessors(), mk_class_scalar_accessors(),
-    mk_concat_accessors(), mk_forward_accessors(), mk_hash_accessors(),
-    mk_integer_accessors(), mk_new(), mk_object_accessors(),
-    mk_scalar_accessors(), mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor(), subname()
-
 =head1 METHODS
 
-=over 4
-
-=item adaptee
-
-    my $value = $obj->adaptee;
-    $obj->adaptee($value);
-
-A basic getter/setter method. If called without an argument, it returns the
-value. If called with a single argument, it sets the value.
-
-=item adaptee_clear
-
-    $obj->adaptee_clear;
-
-Clears the value.
-
-=item clear_adaptee
-
-    $obj->clear_adaptee;
-
-Clears the value.
-
-=item semantic_class_name
+=head2 semantic_class_name
 
 Returns the corresponding semantic class name. This method provides a default
 mapping, the idea of which is to mirror the layout of the Data::Semantic class
@@ -193,25 +111,25 @@ tree. If you have a different mapping, override this method in a subclass.
 So in the Data::Domain::URI::http class, it will return
 C<Data::Semantic::URI::http>.
 
-=item adaptee
+=head2 adaptee
 
 Takes the results of C<semantic_class_name()> and C<semantic_args()>, loads
 the semantic data class and returns a semantic data object with the given args
 passed to its constructor.
 
-=item semantic_args
+=head2 semantic_args
 
 Turns the object's options, specified via C<OPTIONS()>, into arguments to be
 passed to the semantic data object's constructor. Returns a hash.
 
-=item _inspect
+=head2 _inspect
 
 Inspects the data using the C<adaptee()>. See L<Data::Domain> for more
 information. Respects the C<-not_in> option and returns a C<EXCLUSION_SET>
 message, if appropriate. If the adaptee() says that the data is not valid
 under the given options, an C<INVALID> message is returned.
 
-=item install_shortcuts
+=head2 install_shortcuts
 
 This is a convenience function (not method) that installs shortcuts into the
 calling package. It expects a mapping hash whose keys are the shortcuts to be
@@ -236,46 +154,39 @@ Now code that wants to use network-based domain objects can just say:
     my $domain = IPv4(-not_in => [ ... ]);
     $domain->inspect(...);
 
-=back
+=head1 INSTALLATION
 
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<datadomainsemanticadapter> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.01 of L<Data::Domain::SemanticAdapter>.
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-data-domain-semanticadapter@rt.cpan.org>>, or through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Data-Domain-SemanticAdapter>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Data-Domain-SemanticAdapter/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Data-Domain-SemanticAdapter/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
--- lib/Data/Domain/SemanticAdapter/Test.pm
+++ lib/Data/Domain/SemanticAdapter/Test.pm
@@ -1,16 +1,13 @@
-package Data::Domain::SemanticAdapter::Test;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Data::Domain::SemanticAdapter::Test;
+our $VERSION = '1.100840';
+# ABSTRACT: testing Data::Domain objects
 use Hash::Rename;
 use Test::More;
-
-
-our $VERSION = '0.01';
-
-
-use base qw(Data::Semantic::Test);
-
+use parent qw(Data::Semantic::Test);
 
 sub munge_args {
     my ($self, %args) = @_;
@@ -18,28 +15,25 @@ sub munge_args {
     # Our keys have a dash at the beginning - make sure every key has one.
     # This also makes it possible to use Data::Semantic::*::TestData::*
     # classes' data as it is automatically munged into the format we want.
-
     hash_rename %args, code => sub { s/^(?!-)/-/ };
     %args;
 }
 
-
 sub test_is_valid {
     my ($self, $obj, $value, $testname) = @_;
+
     # can be empty string or 0
     like($obj->inspect($value), qr/^0?$/, $testname);
 }
 
-
 sub test_is_invalid {
     my ($self, $obj, $value, $testname) = @_;
+
     # can be empty string or 0
     like($obj->inspect($value), qr/^(\w+)(::\w+)*: invalid$/, $testname);
 }
 
-
 # convenience test methods
-
 sub is_excluded {
     my ($self, $domain, $data) = @_;
     like(
@@ -49,94 +43,44 @@ sub is_excluded {
     );
 }
 
-
 sub is_invalid {
     my ($self, $domain, $data) = @_;
     $self->test_is_invalid($domain, $data, "invalid: $data");
 }
 
-
 sub is_valid {
     my ($self, $domain, $data) = @_;
     $self->test_is_valid($domain, $data, "valid: $data");
 }
-
-
 1;
 
 
 __END__
-
-
+=pod
 
 =head1 NAME
 
 Data::Domain::SemanticAdapter::Test - testing Data::Domain objects
 
-=head1 SYNOPSIS
+=head1 VERSION
 
-    Data::Domain::SemanticAdapter::Test->new;
+version 1.100840
 
 =head1 DESCRIPTION
 
-This class can be used to test classes deriveed from
+This class can be used to test classes derived from
 L<Data::Domain::SemanticAdapter>. It works in conjunction with
 L<Test::CompanionClasses>.
 
-Data::Domain::SemanticAdapter::Test inherits from L<Data::Semantic::Test>.
-
-The superclass L<Data::Semantic::Test> defines these methods and functions:
-
-    PLAN(), run()
-
-The superclass L<Test::CompanionClasses::Base> defines these methods and
-functions:
-
-    new(), clear_package(), make_real_object(), package(), package_clear(),
-    planned_test_count()
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    carp(), cluck(), croak(), flatten(), mk_abstract_accessors(),
-    mk_array_accessors(), mk_boolean_accessors(),
-    mk_class_array_accessors(), mk_class_hash_accessors(),
-    mk_class_scalar_accessors(), mk_concat_accessors(),
-    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
-    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
-    mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor(), subname()
-
-The superclass L<Data::Inherited> defines these methods and functions:
-
-    every_hash(), every_list(), flush_every_cache_by_key()
-
 =head1 METHODS
 
-=over 4
-
-
-
-=item munge_args
+=head2 munge_args
 
 Test data classes usually define C<TESTDATA()> to have arguments without
 leading dashes. This method munges the args to the usual L<Data::Domain> style
 by prepending a dash to those keys that don't already start with a dash.
 
 So if the C<TESTDATA()> looks like this:
-
 
     use constant TESTDATA => (
         {
@@ -150,31 +94,31 @@ So if the C<TESTDATA()> looks like this:
         },
     );
 
-the data domain object to be passed will effectlvely be constructed like this:
+the data domain object to be passed will effectively be constructed like this:
 
     $self->make_real_object('-foo' => 1', '-bar' => 'baz');
 
 C<make_real_object()> comes from L<Test::CompanionClasses::Base>.
 
-=item test_is_valid
+=head2 test_is_valid
 
 Overrides this method by passing the value to be tested to the data domain
 object's C<inspect()> method and checking that it either returns an empty
 string or C<0>.
 
-=item test_is_invalid
+=head2 test_is_invalid
 
 Overrides this method by passing the value to be tested to the data domain
 object's C<inspect()> method and checking that it returns an C<INVALID>
 message as defined in L<Data::Domain>.
 
-=item is_excluded
+=head2 is_excluded
 
 Takes a data domain object and a value to be tested. Passes the value to the
 data domain object's C<inspect()> method and checks whether it returns an
 C<EXCLUSION_SET> message as defined in L<Data::Domain>.
 
-=item is_invalid
+=head2 is_invalid
 
 Takes a data domain object and a value to be tested. Passes the value to the
 data domain object's C<inspect()> method and checks whether it returns an
@@ -185,49 +129,43 @@ while iterating over C<TESTDATA()> and so it gets a test name as an argument,
 while this method can be used for custom tests - it creates its own test
 name.
 
-=item is_valid
+=head2 is_valid
 
-Analoguous to C<is_invalid()>.
-
-=back
-
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<datadomainsemanticadapter> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.01 of L<Data::Domain::SemanticAdapter::Test>.
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<<bug-data-domain-semanticadapter@rt.cpan.org>>, or through the web interface at
-L<http://rt.cpan.org>.
+Analogous to C<is_invalid()>.
 
 =head1 INSTALLATION
 
 See perlmodinstall for information and options on installing Perl modules.
 
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Data-Domain-SemanticAdapter>.
+
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Data-Domain-SemanticAdapter/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Data-Domain-SemanticAdapter/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
+
